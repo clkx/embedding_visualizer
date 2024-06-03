612e30c9280f550e3863804547a7c30491448053
clkx <144937481+clkx@users.noreply.github.com>
2024-06-04 04:49:45 +0800

    Update main.js

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ function createSphere(xPosition, yPosition, zPosition, name, genre, prompt) {
 }
 
 async function fetchData() {
-    const response = await fetch('http://127.0.0.1:8080/fixed');
+    const response = await fetch('https://embedding-visualizer-backend.onrender.com/fixed');
     const data = await response.json();
 
     data.forEach(item => {
